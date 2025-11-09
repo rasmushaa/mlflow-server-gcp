29d801815dd48da4aaa2975162131a2d28938d39
rasmushaa <rasmus.haapaniemi@outlook.com>
2025-11-09 14:23:16 +0200

    Update local mock testing, fix pipeline vars

--- server.sh
+++ server.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
+
 # Use PORT provided by Cloud Run (default 8080)
 PORT=${PORT:-8080}
 
-# Require basic auth credentials
+
+# Ensure Auth Credentials are always provided
 if [ -z "${MLFLOW_TRACKING_USERNAME-}" ] || [ -z "${MLFLOW_TRACKING_PASSWORD-}" ]; then
     echo "MLFLOW_TRACKING_USERNAME and MLFLOW_TRACKING_PASSWORD must be set"
     exit 1
 fi
 
-echo "Creating /etc/nginx/.htpasswd for user '${MLFLOW_TRACKING_USERNAME}'"
+
+# Create the creds-file for nginx
 printf "%s:%s\n" "$MLFLOW_TRACKING_USERNAME" "$(openssl passwd -apr1 "$MLFLOW_TRACKING_PASSWORD")" > /etc/nginx/.htpasswd
 
 
+# Mlflow server
 echo "Starting MLflow server (host=127.0.0.1 port=5000)"
 mlflow server \
     --host 127.0.0.1 \
@@ -21,6 +25,7 @@ mlflow server \
     --backend-store-uri "$BACKEND_STORE_URI" \
     --default-artifact-root "$ARTIFACT_ROOT" \
     --serve-artifacts &
+
 
 # Wait for MLflow to become available before starting nginx
 echo "Waiting for MLflow to become available on http://127.0.0.1:5000"
@@ -32,5 +37,7 @@ for i in {1..30}; do
     sleep 1
 done
 
+
+# Start Reverse Proxy
 echo "Starting nginx in foreground"
 nginx -g "daemon off;"
